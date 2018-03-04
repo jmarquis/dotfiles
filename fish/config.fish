bcac546b77077c7c8bbca3b09ddd41a80e28eec9
Jeremy Marquis <jeremy@jeremymarquis.com>
2018-03-03 21:06:35 -0500

    cleanup

--- fish/config.fish
+++ fish/config.fish
@@ -10,8 +10,3 @@ set -g fish_user_paths "/usr/local/opt/mysql@5.6/bin" $fish_user_paths
 # --follow: Follow symlinks
 # --glob: Additional conditions for search (in this case ignore everything in the .git/ folder)
 set -x FZF_DEFAULT_COMMAND 'rg --files --hidden --follow --glob "!.git/*"'
-
-# Base16 Shell
-if status --is-interactive
-  eval sh $HOME/.config/base16-shell/scripts/base16-default-dark.sh
-end

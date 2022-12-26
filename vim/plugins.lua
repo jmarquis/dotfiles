3efb233669a775115ff162f3f3b03ee79c6cf51e
Jeremy Marquis <jeremy@jeremymarquis.com>
2022-12-26 14:14:13 -0500

    prettier autoformat

--- vim/plugins.lua
+++ vim/plugins.lua
@@ -528,11 +528,31 @@ return packer.startup({ function(use)
   use {
     'jayp0521/mason-null-ls.nvim',
     requires = {
+      'williamboman/mason.nvim',
       'jose-elias-alvarez/null-ls.nvim'
     },
     config = function()
       require('mason-null-ls').setup({
         automatic_setup = true
+      })
+
+      require('mason-null-ls').setup_handlers({})
+
+      local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+      require("null-ls").setup({
+        on_attach = function(client, bufnr)
+          if client.supports_method("textDocument/formatting") then
+            vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+            vim.api.nvim_create_autocmd("BufWritePre", {
+              group = augroup,
+              buffer = bufnr,
+              callback = function()
+                -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+                vim.lsp.buf.format({ bufnr = bufnr })
+              end,
+            })
+          end
+        end,
       })
     end
   }

75000f3de4f8358aa5f634cb914c6b8c2e4d18e1
Jeremy Marquis <jeremy@jeremymarquis.com>
2024-08-13 10:55:44 -0400

    ruby lsp fix

--- vim/lua/plugins/plugins.lua
+++ vim/lua/plugins/plugins.lua
@@ -23,9 +23,13 @@ return {
 
   {
     "neovim/nvim-lspconfig",
-    opts = function()
+    opts = function(_, opts)
       local keys = require("lazyvim.plugins.lsp.keymaps").get()
       keys[#keys + 1] = { "<c-k>", mode = "i", false }
+      opts.servers["ruby_lsp"] = {
+        mason = false,
+        cmd = { vim.fn.expand("~/.rbenv/shims/ruby-lsp") },
+      }
     end,
   },
 

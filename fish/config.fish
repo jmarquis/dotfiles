b1403b3fe6719d4c22f52f57efd2bf093926dd4a
Jeremy Marquis <jmarquis@etsy.com>
2025-05-08 09:51:11 -0400

    vm command

--- fish/config.fish
+++ fish/config.fish
@@ -3,7 +3,7 @@ alias vi="nvim"
 alias vim="nvim"
 alias gdiff="gist -t diff"
 alias g="git"
-alias vm="et vm -c 'tmux a || tmux'"
+alias vm="et -x vm -c 'tmux a || tmux'"
 
 if test -e /opt/homebrew/bin/brew
     eval "$(/opt/homebrew/bin/brew shellenv)"
